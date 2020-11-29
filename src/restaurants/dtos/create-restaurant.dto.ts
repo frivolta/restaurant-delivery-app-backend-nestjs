9647403f07b6b2960972bc6cc52131cef79e8d28
frivolta <rivoltafilippo@gmail.com>
2020-11-30 00:22:49 +0100

    Init validation

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,7 +1,10 @@
 import { ArgsType, Field } from "@nestjs/graphql";
+import { IsString, Length } from "class-validator";
 
 @ArgsType()
 export class CreateRestaurantDto{
   @Field(type => String)
+  @IsString()
+  @Length(5,10)
   name: string
 }
